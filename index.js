e859de4d0e04abe486d0ede97508edd5613198bb
Mohamed Samir <robben.97@hotmail.com>
2021-01-07 20:21:32 +0200

    Mongoose part2

--- index.js
+++ index.js
@@ -9,30 +9,28 @@ const connect = mongoose.connect(url);
 connect.then((db) =>{
     console.log('Connected correctly yo server');
 
-    var newDish = Dishes({
+     Dishes.create({
         name:'Uthappizza',
         description: 'test'
+    })
+    .then((dish) =>{
+        console.log(dish);
+
+        return Dishes.find({}).exec();
+        
+    })
+    .then ((dishes) => {
+        console.log(dishes);
+        return Dishes.remove({});
+
+    })
+    .then(() =>{
+        return mongoose.connection.close();
+        
+    })
+    .catch((err)=> {
+        console.log(err);
+
     });
-
-    newDish.save()
-        .then((dish) =>{
-            console.log(dish);
-
-            return Dishes.find({}).exec();
-            
-        })
-        .then ((dishes) => {
-            console.log(dishes);
-            return Dishes.remove({});
-
-        })
-        .then(() =>{
-            return mongoose.connection.close();
-            
-        })
-        .catch((err)=> {
-            console.log(err);
-
-        });
 
 });
